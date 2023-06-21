f883616166fce66119672c14634fcac91ed95e28
Bruno Duarte <bruno.duarte@projetooaasis.com>
2023-06-21 16:03:02 -0300

    update cart

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 const path = require('path');
-const bodyParser = require("body-parser");
 const session = require('express-session');
 const routes = require('./src/routes/routes.js');
 const routesPag = require('./src/routes/routePag.js');
+const routesCart = require('./src/routes/cart.js');
 
 app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(session({
     secret: '2C44-4D44-WppQ38S',
     resave: true,
     saveUninitialized: true
 }));
-app.use('/', routes, routesPag);
+app.use('/', routes, routesPag, routesCart);
 app.use((req, res) => {
     res.status(404).render('manutencao');
 });
---
+++ src/routes/cart.js
@@ -0,0 +1,22 @@
+const cart = require("express").Router();
+var cartItems = [];
+
+cart.post('/cart', (req, res) => {
+    const item = req.body;
+
+    cartItems.push(item);
+    res.status(201).json({ message: 'Item adicionado ao carrinho' });
+});
+
+cart.get('/listCart', (req, res) => {
+    res.status(200).json(cartItems);
+});
+
+cart.delete('/deleteCart/:itemId', (req, res) => {
+    const itemId = req.params.itemId;
+
+    cartItems = cartItems.filter(item => item.id !== itemId);
+    res.status(202).json({ message: 'Item removido do carrinho' });
+});
+
+module.exports = cart;
